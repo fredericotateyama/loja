aef93633d8119969119c654937bd7a30d28e9a1a
fredericotateyama <fredericotateyama@gmail.com>
2017-06-23 07:00:01 -0300

    Método "post" / ContatoController / Tela "salva-contato"

---
+++ Desktop/Labs/Java/Loja/src/main/java/br/com/fred/web/controle/ContatoController.java
@@ -0,0 +1,58 @@
+package br.com.fred.web.controle;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.fred.web.entidade.Contato;
+import br.com.fred.web.persistencia.ContatoDAO;
+
+@WebServlet("/contatocontroller")
+public class ContatoController extends HttpServlet {
+
+	public ContatoController() {
+		System.out.println("Construtor..");
+	}
+
+	@Override
+	public void init() throws ServletException {
+		System.out.println("Init..");
+		super.init();
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
+	  throws ServletException, IOException {
+		System.out.println("doPost"+ " : " + req);
+		  PrintWriter out = resp.getWriter();
+		  	
+		  	String id  = req.getParameter("id");
+	        String nm  = req.getParameter("nome");
+		    String end = req.getParameter("endereco");
+		    String em  = req.getParameter("email");
+			   	Contato ctt = new Contato();
+			  	   ctt.setNome(nm);
+			  	   ctt.setEndereco(end);
+			  	   ctt.setEmail(em);
+			  	 
+		  	   ContatoDAO dao = new ContatoDAO();
+		  	    dao.salvar(ctt);
+		  	     System.out.println("Salvo!");
+		  	      out.println("<html>");
+				  out.println("<body>");
+			        out.println("Contato:" + " " + ctt.getNome() + " " + " adicionado com sucesso");
+			      out.println("</html>");
+			      out.println("</body>");		  	  
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println("Destroy..");
+		super.destroy();
+	}
+}
